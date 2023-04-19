542eb5313abc2263736894135f09d4177fcea4cc
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-04-19 07:06:08 +0600

    prop drilling and react dev tool

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import CartDetail from '../CartDetail/CartDetail';
 
 const Cart = ({ cart, removeCartItem }) => {
-    // console.log(cart);
+    console.log(cart);
+    let message;
+    // if(cart.length> 2){
+    //     message = <p>Pagoler ek din</p>
+    // }
+
 
 
     return (
@@ -10,6 +15,12 @@ const Cart = ({ cart, removeCartItem }) => {
             <h1 className="text-3xl text-center text-teal-800 font-bold">
                 OrderDetail
             </h1>
+            {
+                    cart.length> 2 ? <span> pagoler ek din</span> : <span> Grihoster dui din</span>
+
+            }
+
+            {message}
 
             {
                 cart.map((cartItem) => <CartDetail
@@ -27,3 +38,17 @@ const Cart = ({ cart, removeCartItem }) => {
 };
 
 export default Cart;
+// CONDITIONAL RENDERING
+// 1..IF ELSE
+// 2..ERNARY OPERATOR
+// 3..OGICAL &&
+// 4..lOGICAL  ||
+
+// Conditional CSS
+// 1.Use Ternary
+// 2.Use template Sting
+
+
+
+
+
